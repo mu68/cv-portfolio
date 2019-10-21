d6138e3f34bbb1157b1e2ebebcd3da3784ebb29d
mu68 <43093319+mu68@users.noreply.github.com>
2019-10-21 15:53:52 +0200

    initialisation du projet sur Github

---
+++ header.php
@@ -0,0 +1,15 @@
+<h1>Développeur,intégrateur web</h1>
+
+<header>
+    <nav>
+        <img class="photo" src="image/moi.png" width=80px height=100px alt="photo de moi">
+            <div class="rond">
+                <ul>
+                    <li class="cercle1 cercle"><a href="Formation/Projet/CV/cv.html"> Acceuil</a></li>
+                    <li class="cercle2 cercle" ><a href="experiences.html"> Expériences</a></li>
+                    <li class="cercle3 cercle"><a href="projets.html"></a> Projets</li>
+                </ul>
+            </div>
+                    
+    </nav>
+</header>
