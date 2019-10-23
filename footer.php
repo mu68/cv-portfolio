020151199681a55f474111c7a2e0d6766d3a55b4
mu68 <sperissen.murielle@orange.fr>
2019-10-23 18:28:10 +0200

    réorganisation des fichiers
    supression de fichier formulaire
    header et footer
    nouvelle disposition

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <p class="fbas"><i class="fas fa-home"></i>9 avenue de la gare 86400 ST-SAVIOL </p>
-    <p class="fbas"><i class="fas fa-phone-alt"></i>06-16-55-16-08</p>
-    <p class="fbas"><i class="fas fa-at"></i>sperissen.murielle@orange.fr</p>
-    <p class="fbas"><a class="cv" href="CV_ Développeur_ Intégrateur_ Web.pdf"><i class="fas fa-newspaper fa-spin"></i>CV
-                    papier</a></p>
-    <p class="fbas"><a class="cv" href="formulairecontact.html"><i class="fas fa-file-signature fa-spin"></i>Formulaire de contact
-                        </a></p>
-</footer>
